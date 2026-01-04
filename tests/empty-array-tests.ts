5423993509ca88be33a55feba3edb8ecdc328529
Edward <yurest3it@gmai;.com>
2026-01-04 16:50:49 +0200

    TL-13: Added Empty tests and changer other params

--- tests/empty-array-tests.ts
+++ tests/empty-array-tests.ts
@@ -1,6 +1,6 @@
 import {test, expect} from '@playwright/test';
 import {StatusCodes} from 'http-status-codes';
-
+//check
 const baseURLWithEndpoint: string = 'http://localhost:3000/users';
 
 test('should return empty array when no users', async ({ request }) => {
