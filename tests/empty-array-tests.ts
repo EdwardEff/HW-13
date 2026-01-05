294f9904a323dc8dae14638e92511476f7f63cb2
Edward <yurest3it@gmai;.com>
2026-01-05 14:31:42 +0200

    TL-14: Added src module for HW14 + Prettier

--- tests/empty-array-tests.ts
+++ tests/empty-array-tests.ts
@@ -1,11 +1,11 @@
-import {test, expect} from '@playwright/test';
-import {StatusCodes} from 'http-status-codes';
+import { test, expect } from "@playwright/test";
+import { StatusCodes } from "http-status-codes";
 //check
-const baseURLWithEndpoint: string = 'http://localhost:3000/users';
+const baseURLWithEndpoint: string = "http://localhost:3000/users";
 
-test('should return empty array when no users', async ({ request }) => {
-    const response = await request.get(`${baseURLWithEndpoint}`);
-    expect(response.status()).toBe(StatusCodes.OK);
-    const responseBody = await response.text()
-    expect(responseBody).toBe('[]');
+test("should return empty array when no users", async ({ request }) => {
+  const response = await request.get(`${baseURLWithEndpoint}`);
+  expect(response.status()).toBe(StatusCodes.OK);
+  const responseBody = await response.text();
+  expect(responseBody).toBe("[]");
 });
